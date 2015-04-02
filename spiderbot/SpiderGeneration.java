bea2c42ff2b7902ed7b54184fad7f472d40f7f64
bk <bk@mark.rams>
2015-04-01 20:58:50 -0400

    refinements made to reflect new Generation class same initial population for every execution

--- spiderbot/SpiderGeneration.java
+++ spiderbot/SpiderGeneration.java
@@ -5,7 +5,7 @@
 //
 package spiderbot;
 
-import geneticalgorithm.Generation;
+import geneticalgorithm.*;
 
 /**
  *
@@ -25,25 +25,46 @@ public class SpiderGeneration extends Generation
     public SpiderGeneration( int pop_size, double mut_rate, int mut_vol, Generation.CrossoverStrategy options)
     {super(pop_size,mut_rate,mut_vol,options);}
     public SpiderGeneration( int pop_size, double mut_rate, int mut_vol,
-    						 Generation.CrossoverStrategy x_strategy, boolean x_random )
-    {super(pop_size,mut_rate,mut_vol,x_strategy,x_random);}
+    						 Generation.CrossoverStrategy x_strategy, Generation.CrossoverPoint x_pt )
+    {super(pop_size,mut_rate,mut_vol,x_strategy,x_pt);}
     
-    //
-    // init the SpiderGeneration
-    //
+    
     @Override
-    protected void init()
+    protected Chromosome[] initial_population( int n )
     {
-        int i;
-        
-        // set the fitness function
-        this.ff = new SpiderBotFitnessFunction();
-
-        // generate a random spider population
-        String[] pop_s = randomPopulation( this.population_size, 27 );
-        for (i = 0; i < this.population_size; i++)
+    	SpiderChromosome[] y = new SpiderChromosome[n];
+    	
+    	// generate a known pattern of initial chromosomes
+    	
+    	// an array of motor speeds
+    	int[] speeds = new int[]{ 17, 37, 67, 107, 157, 167, 197, 227 };
+    	
+    	// k is the index into the speed array that will increment at a factorial rate
+    	int k = 0, incr = 1;
+    	boolean dir = false; // for the direction, will toggle at each iteration
+    	
+    	// over all the population...
+        for (int i = 0; i < n; i++)
         {
-            this.population[i] = new SpiderChromosome( pop_s[i] );
+        	String chr_s = "";
+        	for (int j = 0; j < 3; j++)
+        	{
+        		// generate a value
+        		int val = (dir ? 256 : 0) + speeds[k%8];
+        		chr_s += BitString.dec2bin(val,9);
+        		// iterate
+        		dir = !dir;
+        		k += incr;
+        		incr += 3;
+        	}
+            y[i] = new SpiderChromosome(chr_s);
         }
+        return y;
+    }
+    
+    @Override
+    protected FitnessFunction fitness_function()
+    {
+    	return new SpiderBotFitnessFunction();
     }
 }
